3f64f9a39bedf6a02071395697e4ec32b724b52b
iamverycute <sa@iamverylovely.com>
2022-04-06 10:22:12 +0800

    删除无关代码^_^

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Toggle Airplane Mode
+
+**基于sdk27 android hidden api构建的飞行模式开关DEMO**
--- app/src/main/java/com/iamverycute/air/SetAirModeActivity.java
+++ app/src/main/java/com/iamverycute/air/SetAirModeActivity.java
@@ -1,11 +1,12 @@
 package com.iamverycute.air;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.net.IConnectivityManager;
 import android.os.Bundle;
-import android.os.RemoteException;
-import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -14,39 +15,33 @@ import rikka.shizuku.Shizuku;
 import rikka.shizuku.ShizukuBinderWrapper;
 import rikka.shizuku.SystemServiceHelper;
 
-import android.content.pm.IPackageManager;
-import android.net.IConnectivityManager;
-
-
-public class SetAirModeActivity extends Activity implements Shizuku.OnRequestPermissionResultListener {
+public class SetAirModeActivity extends AppCompatActivity implements Shizuku.OnRequestPermissionResultListener, CompoundButton.OnCheckedChangeListener {
 
     private final IConnectivityManager icm = IConnectivityManager.Stub.asInterface(new ShizukuBinderWrapper(SystemServiceHelper.getSystemService(Context.CONNECTIVITY_SERVICE)));
-    private final IPackageManager ipm = IPackageManager.Stub.asInterface(new ShizukuBinderWrapper(SystemServiceHelper.getSystemService("package")));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        try {
-            if (Shizuku.pingBinder()) {
-                if (Shizuku.checkSelfPermission() != PackageManager.PERMISSION_GRANTED) {
-                    Shizuku.requestPermission(0);
-                }
+        setContentView(R.layout.main);
+        Switch aSwitch = findViewById(R.id.switch2);
+        aSwitch.setOnCheckedChangeListener(this);
+        if (Shizuku.pingBinder()) {
+            if (Shizuku.checkSelfPermission() != PackageManager.PERMISSION_GRANTED) {
+                Shizuku.requestPermission(0);
             }
-            ipm.grantRuntimePermission(getPackageName(), android.Manifest.permission.WRITE_SECURE_SETTINGS, 0);
-            boolean isClosed = Settings.Global.getInt(getContentResolver(), Settings.Global.AIRPLANE_MODE_ON) == 0;
-            Settings.Secure.putInt(getContentResolver(), Settings.Secure.LOCATION_MODE, 0);
+        }
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        try {
             Method Airplane = icm.getClass().getMethod("setAirplaneMode", boolean.class);
-            Airplane.invoke(icm, isClosed);
-        } catch (Settings.SettingNotFoundException | NoSuchMethodException | IllegalAccessException | InvocationTargetException | RemoteException e) {
-        } catch (Exception e) {
-        } finally {
-            finish();
-            System.exit(0);
+            Airplane.invoke(icm, isChecked);
+        } catch (InvocationTargetException | IllegalAccessException | NoSuchMethodException ignored) {
         }
     }
 
     @Override
     public void onRequestPermissionResult(int requestCode, int grantResult) {
-
     }
 }
